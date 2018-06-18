fe0e99e5da63933238bd6e358f16b75e8874d314
Grzegorz Jasiński <gjasinski@hotmail.com>
2018-06-18 23:17:01 +0200

    Remove api-key

--- src/main/java/com/tai/git/collector/PomCleaner.java
+++ src/main/java/com/tai/git/collector/PomCleaner.java
@@ -22,12 +22,54 @@ class PomCleaner {
 				.map(pomDownloader::downloadRawPage)
 				.filter(pom -> !containsEmptyDependencies(pom))
 				.filter(this::containsDependenciesTag)
+				.map(this::removeVariables)
 				.map(this::extractDependecies)
 				.flatMap(splitDependencies());
 	}
 
 	private Function<String, Stream<? extends String>> splitDependencies() {
 		return dependencies -> Arrays.stream(dependencies.split(DEPENDENCY_CLOSING_TAG));
+	}
+
+	private String removeVariables(String pom) {
+		int loop = pom.split("<version>\\$\\{").length;
+		System.out.println("LOOP + " + loop);
+		for (int i = 1; i < loop; i++) {
+			String variable = extractVariable(pom);
+			/*if(variable.equals("project.version")){
+				variable = "version";
+			}*/
+			if (containsVariable(pom, variable)) {
+				String variableValue = extractVariableValue(pom, variable);
+				pom = swapVariableWithVariableValue(pom, variable, variableValue);
+			}
+			else {
+				pom = swapVariableWithVariableValue(pom, variable, "");
+			}
+		}
+		return pom;
+	}
+
+	private boolean containsVariable(String pom, String variable) {
+		return pom.contains(variable + ">") && pom.contains("</" + variable);
+	}
+
+	private String extractVariable(String pom) {
+		String tmp = pom.substring(pom.indexOf("<version>${") + 11);
+		System.out.println("BEEEDE WYPISYWAL" + tmp);
+		try {
+			return tmp.substring(0, tmp.indexOf('}'));
+		}catch (Exception e){
+			throw new IllegalArgumentException(tmp);
+		}
+	}
+
+	private String extractVariableValue(String pom, String variable) {
+		return pom.substring(pom.indexOf(variable + ">") + variable.length() + 1, pom.indexOf("</" + variable));
+	}
+
+	private String swapVariableWithVariableValue(String pom, String variable, String variableValue) {
+		return pom.replace("${" + variable + "}", variableValue);
 	}
 
 	private String extractDependecies(String pom) {
--- src/main/java/com/tai/git/collector/PomSearcher.java
+++ src/main/java/com/tai/git/collector/PomSearcher.java
@@ -12,12 +12,10 @@ import org.springframework.web.client.RestTemplate;
 
 public class PomSearcher {
 	private static final String GUTHUB_API_PREFIX = "https://api.github.com/search/code?q=pom.xml+in:path+user:";
-	//todo this token can not be stored like that! its fake account
-	private static final String ACCESS_TOKEN = "&access_token=d85c15341fbb35f61c67aab302f7ee4640b28e5c";
 	private final RestTemplate restTemplate = new RestTemplate();
 
 	QueryResultDTO[] downloadPOMForUser(String user) {
-		String fooResourceUrl = GUTHUB_API_PREFIX + user + ACCESS_TOKEN;
+		String fooResourceUrl = GUTHUB_API_PREFIX + user;
 		HttpHeaders headers = buildHeaders();
 		HttpEntity<?> entity = new HttpEntity<>(headers);
 		ResponseEntity<QueryResultsDTO<QueryResultDTO>> exchange = restTemplate.exchange(
